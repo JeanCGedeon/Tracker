8cf0182131d9c87aa595cf843c9b608f7c3e865f
Jean Gedeon <carlenssg@gmail.com>
2022-07-30 05:03:08 -0400

    fixed rootRouter heroku

--- server/src/routes/rootRouter.js
+++ server/src/routes/rootRouter.js
@@ -2,13 +2,13 @@ import express from "express";
 import userSessionsRouter from "./api/v1/userSessionsRouter.js";
 import usersRouter from "./api/v1/usersRouter.js";
 import clientRouter from "./clientRouter.js";
-import habitsRouter from "./api/v1/HabitsRouter.js";
+import habitsRouter from "./api/v1/habitsRouter.js";
 import logHabitsRouter from "./api/v1/logHabitsRouter.js";
 const rootRouter = new express.Router();
 rootRouter.use("/", clientRouter);
 
 rootRouter.use("/api/v1/user-sessions", userSessionsRouter);
-rootRouter.use("/api/v1/users", usersRouter); //place your server-side routes here
+rootRouter.use("/api/v1/users", usersRouter);
 rootRouter.use("/api/v1/habits", habitsRouter)
 rootRouter.use("/api/v1/logs",logHabitsRouter )
 export default rootRouter;
