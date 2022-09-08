62bcd8e5b90a29fea920be6cb0c18b3699b2da83
Jean Gedeon <carlenssg@gmail.com>
2022-09-08 14:51:29 -0400

    fix again

--- server/src/models/Comment.js
+++ server/src/models/Comment.js
@@ -8,7 +8,7 @@ class Comment extends Model {
   static get jsonSchema() {
     return {
       type: "object",
-      required: ["userId","habitId"],
+      required: [],
       properties: {
         comment: { type: "string" },
         // user: { type: "string" },
